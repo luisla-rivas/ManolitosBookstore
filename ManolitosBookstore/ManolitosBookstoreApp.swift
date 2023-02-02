9473dd373cb69a205cedd3b9201cf8a2f8ab7f3d
Luis Studio <luisla.tester@luisla.com>
2023-02-02 15:54:19 +0100

    Initial Commit

---
+++ ManolitosBookstore/ManolitosBookstoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  ManolitosBookstoreApp.swift
+//  ManolitosBookstore
+//
+//  Created by Luis Lasierra on 2/2/23.
+//
+
+import SwiftUI
+
+@main
+struct ManolitosBookstoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
